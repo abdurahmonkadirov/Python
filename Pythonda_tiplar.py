4ce61484a1d0b7574df5fbe8336460ac7d759913
abdurahmonkadirov <95974812+abdurahmonkadirov@users.noreply.github.com>
2021-12-12 09:40:30 +0500

    Pythonda tiplar
    Pythonda tiplar ya'ni o'zgaruvchilar turlari.

---
+++ Pythonda_tiplar.py
@@ -0,0 +1,17 @@
+# Python juda ko'p tiplar mavjud
+#Maslan:
+#str-o'zgaruvchining yozma shakli. bi qo'shtirnoq ichiga yozadigan nars str:"Salom men abdurahmonman"
+#int-Butun sonlar:1,2,3,4,5,6,7,8,9,10...
+#set-Tartiblangan royhat
+#setfrozen-set bilan bir xil faqat tartiblanmaydi.Nomi ham frozen ya'ni muzlatilgan
+#bool-Mantiqiy qiymat True yoki False qaytardi
+#dict-Lug'at bu xuddi a=16 o'xshaydi ammo shu narsani bir o'zgaruvchi saqlaydi.Misol:b={"a":16}
+#list-royhatlar bir o'zgaruvchini bir necha qiymatga tenglaydi.
+#Har biriga misollar
+a="Salom" #str-string-matn
+b=48 #int-Butun son
+c={"olma","behi"}
+d=False #Mantiqiy qiymat
+e={"Salom":"Xayr","Xayr":"Salom"}
+f=['kim',True,False,'Salom',55] # lug'at
+
