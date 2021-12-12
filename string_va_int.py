fd76bbcab85f8e7867104b586ef5ea62125ea3d6
abdurahmonkadirov <95974812+abdurahmonkadirov@users.noreply.github.com>
2021-12-12 09:54:29 +0500

    string_int_float
    string int va float tiplari

---
+++ string_va_int.py
@@ -0,0 +1,8 @@
+#string,int va float 
+#Pythondagi string,int va float
+a=10 #int tipi
+b="salom" #str tipi
+c=10.2322 #float tipi-o'nlik sonlar butun emas 
+print(a) #10ni qaytaradi
+print(b) #"Salom"ni qaytaradi
+print(c) #"10.2322"ni qaytaradi
