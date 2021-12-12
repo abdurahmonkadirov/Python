298222098694c364124c253cef9d03806c5da9db
abdurahmonkadirov <95974812+abdurahmonkadirov@users.noreply.github.com>
2021-12-12 10:11:41 +0500

    stringning funksiyalari
    stringning funksiyalari.

---
+++ string_Funksiyalari.py
@@ -0,0 +1,14 @@
+print('Hello World!)
+#Hello Word qaytaradi
+print('Hellow \tWord!')
+#Hello      Word!
+print('Hello \nWord!')
+#Hello keyingi qator Word! qaytaradi!
+ism='anvar'
+#Siz 'ANVAR' deb chiqarmoqchisiz.Buning uchun o'zgaruvchini o'zgartirish shart emas,balki ism.upper() deb yozish kerak xolos
+print(ism.upper())
+familiya="QODIROV"		
+#Siz 'qodirov' deb chiqarmoqchisiz o'zgaruvchini o'zgartirish shart emas,balki familiya,lower() qilsa bo'ldi
+print(familiya.lower())
+#Siz ism o'zgaruvchisini birinchi harfini katta qilmoqcisiz bunda ism.title() qilsa bas.
+print(ism.title())
