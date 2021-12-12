ed5550ac3709799ecffbcc15aa7f37edd11cd952
abdurahmonkadirov <95974812+abdurahmonkadirov@users.noreply.github.com>
2021-12-12 09:28:38 +0500

    Create O'zgaruvchilar.py
    Pythonning asosiy qismi o'zgaruvchilar darsi.

---
+++ O'zgaruvchilar.py
@@ -0,0 +1,11 @@
+#Pythonda o'zgaruvchilar
+#Bunga misol:
+a=10 # a endi bizning kodimizda doimiy 10ga teng.
+print(a) #Agar uni print qilsak a emas balki 10 chiqadi.Buni qo'shtirnoqlarsiz amalga oshiring!
+#Agar a="Salom" qilsang endigi gal "Salom" so'zi chiqadi.Chunki biz a ga boshqa qiymat berdik.
+#O'zgaruvchilarning en qulay tomoni Bir narsani qisqartirib uni bir necha marta takrorlash mumkin!
+#Masalan:
+a="Salom bugun soat yettida uyga ketaman"
+print(a*10) # Bu usul a dagi qiymatni 10marta takror qaytaradi.
+
+
