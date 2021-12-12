a1926553e4dba25e3ba6203665a5faa440e0deb8
abdurahmonkadirov <95974812+abdurahmonkadirov@users.noreply.github.com>
2021-12-12 11:29:22 +0500

    lists
    royhatlar

---
+++ lists.py
@@ -0,0 +1,15 @@
+#Pythonda royhat
+a=["Salom","Xayr"]
+print(a)
+#royhatda index degan narsa bor bu royhatda qaysi element qaysi joyda turganinni bildiradi.
+#Masalan "Salom" [0] indeks,"Xayr" [1] indeks
+print(a[0]) #"Salom" shunday tarzda chiqariladi
+print(a[1]) #"Xayr" shunday tarzda chiqariladi
+#Demak indeks 0dan boshlanadi buni esdan chiqarmang!!!
+#len metodi royhatni uzunligini aniqlaydi
+print(len(a))
+#royhatni tartiblash mumkin sort yoki sorted
+#royhatdan elemnt olish uchun pop methodidan foydalanasiz
+a.pop(0) # 0-elementni sug'urib olish
+
+
